85079fbd7b07ceeb0c8e05f1a2493d41dfc1d63b
Jon McCarthy <jtmcc93@gmail.com>
2018-05-08 18:05:53 -0700

    add CRUD actions to tasks controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,12 +1,49 @@
 module Api::V1
     class TasksController < ApplicationController
+        before_action :set_task, only: [:show, :update, :destroy]
 
         def index 
             @tasks = Task.all 
 
             render json: @tasks#.to_json(only: [:name, :priority, :due_date])
         end
-        
+
+        def show
+            render json: @task 
+        end
+
+        def create
+            @task = Task.new(task_params)
+
+            if @task.save
+                render json: @task, status: :created
+            else
+                render json: @task.errors, status: :unprocessable_entity
+            end
+        end
+
+        def update
+            if @task.update(task_params)
+                render json: @task 
+            else
+                render json: @task.erros, status: :unprocessable_entity
+            end
+        end
+
+        def destroy
+            @task.destroy
+        end
+        private
+
+        def set_task
+            @task = Task.find(params[:id])
+        end
+
+        def task_params
+            params.requare(:task).permit(:name, :priority, :due_date)
+        end
+
+    
     end
 end
  
