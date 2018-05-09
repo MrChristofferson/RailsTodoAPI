573872fed9fc81ff62e297f6e6f6afdbde895a59
Jon McCarthy <jtmcc93@gmail.com>
2018-05-08 18:26:18 -0700

    add CRUD actions to Users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,30 @@
-class UsersController < ApplicationController
-end
+module Api::V1
+    class UsersController < ApiController
+    before_action :set_user, only: [:show, :update, :destroy] def index
+    @users = User.all render json: @users
+    end
+    def show
+    render json: @user
+    end
+    def create
+    @user = User.new(email: params[:user][:email], password: params[:user][:password]) if @user.save
+    render
+    else
+    render
+    end end
+    def update
+    json: @user, status: :created
+    json: @user.errors, status: :unprocessable_entity
+    if @user.update(user_params) render json: @user, status: :ok
+    else
+    render json: @user.errors, status: :unprocessable_entity end
+    end
+    def destroy @user.destroy
+    end
+    private
+    def set_user
+    @user = User.find(params[:id])
+    end
+    def task_params params.require(:user).permit(:email, :password)
+    end end 
+    end  
