e5d436a575c91f0939ce211f6335e65d57e37d82
Jon McCarthy <jtmcc93@gmail.com>
2018-05-08 18:44:19 -0700

    fomat user contoller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,30 +1,46 @@
 module Api::V1
-    class UsersController < ApiController
-    before_action :set_user, only: [:show, :update, :destroy] def index
-    @users = User.all render json: @users
+  class UsersController < ApiController
+    before_action :set_user, only: %i[show update destroy]
+
+    def index
+      @users = User.all
+      render json: @users
     end
+
     def show
-    render json: @user
+      render json: @user
     end
+
     def create
-    @user = User.new(email: params[:user][:email], password: params[:user][:password]) if @user.save
-    render
-    else
-    render
-    end end
+      @user = User.new(email: params[:user][:email], password: params[:user][:password])
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
     def update
-    json: @user, status: :created
-    json: @user.errors, status: :unprocessable_entity
-    if @user.update(user_params) render json: @user, status: :ok
-    else
-    render json: @user.errors, status: :unprocessable_entity end
+      if @user.update(user_params)
+        render json: user, status: :ok
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
-    def destroy @user.destroy
+
+    def destroy
+      @user.destroy
     end
+
     private
+
     def set_user
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
-    def task_params params.require(:user).permit(:email, :password)
-    end end 
-    end  
+
+    def user_parmas
+      params.require(:user).permit(:emial, :password)
+    end
+    
+  end
+end
